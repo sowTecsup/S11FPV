71889e699d8c7ce3ef1d06f2b2a927241a56f5ad
mmontgomerytecsup <jneyra@tecsup.edu.pe>
2025-10-31 11:51:06 -0500

    Com

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private string[] mapaDemoV1 = new string[] { "Bosque", "Piedra", "Personaje", "Piedra" };
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+/*
+ 
+private string[] mapaDemoV1 = new string[] { "Bosque", "Piedra", "Personaje", "Piedra" };
     // private string[,] mapaDemoV2 = new string[10,4];
     private string[,] mapaDemoV2 = new string[,] 
     {
@@ -22,47 +38,41 @@ public class GameManager : MonoBehaviour
         /*for (int i = 0; i < mapaDemoV1.Length; i++)//4
         {
             print(mapaDemoV1[i]);
-        }*/
-        //print(mapaDemoV2[0, 0])
-
-        /*for (int i = 0; i < mapaDemoV2.GetLength(0); i++)
-        {
-            //print(mapaDemoV2[i, 0]);
-            for (int j = 0; j < mapaDemoV2.GetLength(1); j++)
-            {
-                print(mapaDemoV2[i, j]);
-            }
-        }*/
-
-
-
-        //print(mapaDemoV3.Length);
-
-        int countElements = 0;
-        for (int i = 0; i < mapaDemoV3.Length; i++)
-        {
-            /*for (int j = 0; j < mapaDemoV3.Length; j++)
-            {
-
-            }*/
-            countElements += mapaDemoV3[i].Length;
         }
-        print("La cantidad total de elementos es :" + countElements);
+//print(mapaDemoV2[0, 0])
 
-
-        for (int i = 0; i < mapaDemoV3.Length; i++)
-        {
-            for (int j = 0; j < mapaDemoV3[i].Length; j++)
-            {
-                print(mapaDemoV3[i][j]);
-            }
-
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
+for (int i = 0; i < mapaDemoV2.GetLength(0); i++)
+{
+    //print(mapaDemoV2[i, 0]);
+    for (int j = 0; j < mapaDemoV2.GetLength(1); j++)
     {
-        
+        print(mapaDemoV2[i, j]);
     }
 }
+
+
+
+//print(mapaDemoV3.Length);
+
+int countElements = 0;
+for (int i = 0; i < mapaDemoV3.Length; i++)
+{
+    for (int j = 0; j < mapaDemoV3.Length; j++)
+    {
+
+    }
+    countElements += mapaDemoV3[i].Length;
+}
+print("La cantidad total de elementos es :" + countElements);
+
+
+for (int i = 0; i < mapaDemoV3.Length; i++)
+{
+    for (int j = 0; j < mapaDemoV3[i].Length; j++)
+    {
+        print(mapaDemoV3[i][j]);
+    }
+
+}
+    }
+*/
